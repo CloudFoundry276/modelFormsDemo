a5b74c9b57c0db2cfdf5270f7ed5351715384780
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-02-18 01:25:14 +0530

    model forms demo done

--- modelFormsApp/views.py
+++ modelFormsApp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from modelFormsApp.forms import ProjectForm
+from modelFormsApp.models import Project
+
 
 # Create your views here.
+def index(request):
+    return render(request, "modelFormsApp/index.html")
+
+
+def listProjects(request):
+    projects_list = Project.objects.all()
+    return render(request, "modelFormsApp/project_list.html", {"projects": projects_list})
+
+
+def addProject(request):
+    form = ProjectForm()
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return index(request)
+    return render(request, "modelFormsApp/project_add.html", {"form": form})
--- modelFormsDemo/urls.py
+++ modelFormsDemo/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from modelFormsApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('project-list/', views.listProjects),
+    path('project-add/', views.addProject),
 ]
